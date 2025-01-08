ccceae3a9d457654fabe2ff4d9364efaa25e878b
Dmitry <dimasel@gmail.com>
2025-01-08 13:30:26 +0700

    Create new project multithreading

---
+++ App/MultithreadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultithreadingApp.swift
+//  Multithreading
+//
+//  Created by Dmitriy Eliseev on 08.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MultithreadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
