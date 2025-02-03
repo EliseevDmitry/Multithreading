3013ccc78d2741550165ae42b445d60e03c297f3
Dmitry <dimasel@gmail.com>
2025-02-04 02:55:02 +0700

    StructClassActor

---
+++ StructClassActor/StructClassActor.swift
@@ -0,0 +1,18 @@
+//
+//  StructClassActor.swift
+//  Multithreading
+//
+//  Created by Dmitriy Eliseev on 03.02.2025.
+//
+
+import SwiftUI
+
+struct StructClassActor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StructClassActor()
+}
