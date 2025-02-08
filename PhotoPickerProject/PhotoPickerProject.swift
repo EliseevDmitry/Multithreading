7c388a73ca22117d9b1d39320421dd2ca518e484
Dmitry <dimasel@gmail.com>
2025-02-08 20:07:17 +0700

    PhotoPicker

--- PhotoPickerProject/PhotoPickerProject.swift
+++ PhotoPickerProject/PhotoPickerProject.swift
@@ -6,10 +6,108 @@
 //
 
 import SwiftUI
+//библиотека для фотопикера на SUI
+import PhotosUI
+
+@MainActor
+final class PhotoPickerProjectViewModel: ObservableObject {
+    @Published private(set) var selectedImage: UIImage? = nil
+    //нельзя делать другой тип данных - потому что мы не знаем что выберет пользователь (фото или видео)
+    @Published var imageSelection: PhotosPickerItem? = nil {
+        didSet {
+            setImage(from: imageSelection)
+        }
+    }
+    
+    @Published private(set) var selectedImages: [UIImage] = []
+    @Published var imagesSelection: [PhotosPickerItem] = [] {
+        didSet {
+            setImages(from: imagesSelection)
+        }
+    }
+    
+    private func setImage(from selection: PhotosPickerItem?) {
+        guard let selection else { return }
+        /*
+         Без пробрасывания ошибки
+         Task {
+         if let data = try? await selection.loadTransferable(type: Data.self){
+         if let uiImage =
+         UIImage(data: data) {
+         selectedImage = uiImage
+         }
+         }
+         }
+         */
+        
+        Task{
+            do{
+                let data = try? await selection.loadTransferable(type: Data.self)
+                guard let data, let image = UIImage(data: data) else {
+                    throw URLError(.badServerResponse)
+                }
+                selectedImage = image
+            } catch {
+                print(error)
+            }
+            
+        }
+    }
+    
+    //функция без проверок (для выбора [] фотографий)
+    private func setImages(from selections: [PhotosPickerItem]) {
+        Task{
+            var images: [UIImage] = []
+            for selection in selections {
+                if let data = try? await selection.loadTransferable(type: Data.self) {
+                    if let image = UIImage(data: data) {
+                        images.append(image)
+                    }
+                }
+            }
+            selectedImages = images
+        }
+    }
+    
+}
 
 struct PhotoPickerProject: View {
+    @StateObject private var viewModel = PhotoPickerProjectViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+            Text("Test Photo & Photos")
+                .padding(.top, 20)
+            if let image = viewModel.selectedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(10)
+            }
+            PhotosPicker(selection: $viewModel.imageSelection, matching: .images) {
+                Text("Выбери фото!")
+            }
+            if !viewModel.selectedImages.isEmpty {
+                ScrollView{
+                    HStack (spacing: 10){
+                        ForEach (viewModel.selectedImages, id: \.self) {image in
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(10)
+                        }
+                    }
+                }
+                .frame(width: .infinity, height: 50)
+            }
+        
+            //matching - тип файлов (.all и .any - можно настраивать)
+            PhotosPicker(selection: $viewModel.imagesSelection, matching: .images) {
+                Text("Выбери фото!")
+            }
+            Spacer()
+        }
     }
 }
 
